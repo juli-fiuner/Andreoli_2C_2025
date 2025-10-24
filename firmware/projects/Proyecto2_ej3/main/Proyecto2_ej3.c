3cd8a96c21e2959df12a89f397a005e80d72aee3
Julieta Andreoli <julieta.andreoli@ingenieria.uner.edu.ar>
2025-10-24 13:48:20 -0300

    Documentación de los ejercicios 1, 2 y 3 del proyecto 2. 24/10

--- firmware/projects/Proyecto2_ej3/main/Proyecto2_ej3.c
+++ firmware/projects/Proyecto2_ej3/main/Proyecto2_ej3.c
@@ -1,25 +1,39 @@
-/*! @mainpage Proyecto 2 ej 3. Interrupciones y puerto serie
+/*! @mainpage Proyecto 2 ejercicio 3. Interrupciones y puerto serie
  *
  * @section genDesc General Description
- *
- * This section describes how the program works.
- *
- * <a href="https://drive.google.com/...">Operation Example</a>
+ * 
+ *      Esta aplicación mide la distancia a la que se encuentra un objeto situado frente al sensor HC-SR04 y da evidencia de esta medida de 2 manerass:
+ * - LEDs: enciende cierta cantidad de LEDs según la distancia medida. Si es menor a 10 cm, se apagan todos los LEDs; si está entre 10 y
+ * 20 cm, enciende el 1er LED; si está entre 20 y 30cm, el 1er y 2ndo LED; y si es mayor a 30cm se prenden los 3 LEDs.
+ * - Pantalla LCD, donde muestra el valor númerico medido.
+ * - A través de una terminal en la PC (enviando los datos por un puerto serie).
+ *      Además, con la tecla 1/letra 'O' se puede activar y desactivar la medición y con la tecla 2/letra 'H' la pantalla 'congela' el valor.
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * |    Peripheral      |   ESP32   	|
+ * |:------------------:|:--------------|
+ * | 	ECHO	    	| 	GPIO_3		|
+ * | 	TRIGGER	    	| 	GPIO_2		|
+ * | 	+5V 	    	| 	+5V 		|
+ * | 	GND 	 	    | 	GND 		|
+ * | 	PIN_SEL_3	 	| 	GPIO_9		|
+ * | 	PIN_SEL_2	 	| 	GPIO_18		|
+ * | 	PIN_SEL_1	 	| 	GPIO_19		|
+ * | 	PIN_D1	 		| 	GPIO_20		|
+ * | 	PIN_D2	 		| 	GPIO_21		|
+ * | 	PIN_D3	 		| 	GPIO_22		|
+ * | 	PIN_D4	 		| 	GPIO_23		|
  *
- *
+ * 
+ * 
  * @section changelog Changelog
  *
- * |   Date	    | Description                                    |
- * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * |   Date	    | Description                                        |
+ * |:----------:|:---------------------------------------------------|
+ * | 24/10/2025 | Entrega de proyecto		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Andreoli Aguilar, Julieta
  *
  */
 
@@ -38,18 +52,74 @@
 #include "uart_mcu.h"
 
 /*==================[macros and definitions]=================================*/
-#define CONFIG_PERIOD_TECLA2_US 1000000 /*1seg*/
+/** @brief Período de interrupción del temporizador A en microsegundos (= 1 seg)
+ */
 #define CONFIG_PERIOD_TECLA1_US 1000000
 
 /*==================[internal data definition]===============================*/
+/** @def TaskHandle_t led1_task_handle
+ *  @brief Handle de la tarea que controla los LEDs y la pantalla LCD.
+ */
 TaskHandle_t led1_task_handle = NULL;
-uint8_t tecla, variable_control_1=0, variable_control_2=0;
+/** @def TaskHandle_t tecla_task_handle
+ * @brief Handle de la tarea que maneja la lectura de las teclas (junto con las variables de control).
+ */
+TaskHandle_t tecla_task_handle = NULL;
+/** @def uint8_t tecla
+ * @brief variable que almacena la tecla leída.
+ */
+uint8_t tecla;
+/** @def uint8_t variable_control_1
+ * @brief se encarga de detener o retomar la medición.
+ * si es 0 --> medicion activa; si es 1 --> medición pausada.
+ */
+uint8_t variable_control_1=0;
+/** @def uint8_t variable_control_1
+ * @brief controla la actualización del valor en la pantalla LCD.
+ * si es 0 --> mantiene el valor actualizado; si es 1 --> congela el valor .
+ */
+uint8_t variable_control_2=0;
+/** @def uint16_t medida
+ * @brief variable que almacena la última distancia medida.
+ */
 uint16_t medida =0;
 
 /*==================[internal functions declaration]=========================*/
 
-void FuncTimerA(void* param){ /*funcion de atencion de interrupcion*/
-    vTaskNotifyGiveFromISR(led1_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_1 que caducó el timer */
+/** @fn static void controlar_by_medida(void *pvParameter)
+*  @brief Controla los LEDs y la pantalla LCD según la distancia medida por el sensor.
+* @return
+*/
+static void controlar_by_medida(void *pvParameter);
+
+/** @fn void FuncTimerA(void* param)
+*  @brief función de atención de interrupción, envía una notificación a la tarea asociada al LED_1 que caducó el timer
+* @return 
+*/
+void FuncTimerA(void* param);
+
+/** @fn void Leertecla_UART(void *param)
+*  @brief función de atención de interrupción de la UART, modifica las variables de control 1 y 2
+* @return
+*/
+void Leertecla_UART(void *param);
+
+/** @fn void leer_tecla1()
+*  @brief Callback de interrupción de Switch_1, invierte la variable de control 1
+* @return
+*/
+void leer_tecla1();
+
+
+/** @fn void leer_tecla2()
+*  @brief Callback de interrupción de Switch_2, invierte la variable de control 2
+* @return
+*/
+void leer_tecla2();
+
+
+void FuncTimerA(void* param){
+    vTaskNotifyGiveFromISR(led1_task_handle, pdFALSE);
 }
 
 void Leertecla_UART(void *param){
